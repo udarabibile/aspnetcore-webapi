ae64a5d66ab9b9afec87cfcecb657bf6dd4b4238
Udara Bibile <bibile.13@cse.mrt.ac.lk>
2019-10-28 08:12:54 +0530

    first commit

---
+++ webapi/Models/UserDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace webapi.Models
+{
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+            LoadDefaultUsers();
+        }
+
+        public List<User> getUsers()
+        {
+            return Users.Local.ToList<User>();
+        }
+
+        private void LoadDefaultUsers()
+        {
+            Users.Add(new User());
+            Users.Add(new User());
+        }
+
+        // public UserDbContext() { }
+    }
+}
