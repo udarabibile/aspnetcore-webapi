d175428afac82b25e7acf18562e5d998f4d5c047
Udara Bibile <bibile.13@cse.mrt.ac.lk>
2019-12-06 08:08:27 +0530

    add initial uow

---
+++ webapi/Repositories/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using webapi.Models;
+
+namespace webapi.Repositories
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Author> AuthorRepository { get; }
+        IRepository<Book> BookRepository { get; }
+        void Save();
+    }
+}
---
+++ webapi/Repositories/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using webapi.Models;
+
+namespace webapi.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext _databaseContext;
+        private IRepository<Author> _authorRepository;
+        private IRepository<Book> _bookRepository;
+        public UnitOfWork(DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        public IRepository<Author> AuthorRepository
+        {
+            get
+            {
+                return _authorRepository = _authorRepository ?? new Repository<Author>(_databaseContext);
+            }
+        }
+
+        public IRepository<Book> BookRepository
+        {
+            get
+            {
+                return _bookRepository = _bookRepository ?? new Repository<Book>(_databaseContext);
+            }
+        }
+
+        public void Save()
+        {
+            _databaseContext.SaveChanges();
+        }
+    }
+}
