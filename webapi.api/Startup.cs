2a9b73b51697b15bd410da28ec906c1cce0a0f43
Udara Bibile <bibile.13@cse.mrt.ac.lk>
2019-12-23 18:00:10 +0530

    move core -> root

--- webapi.api/Startup.cs
+++ webapi.api/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-using webapi.core;
+using webapi.root;
 
 namespace webapi.api
 {
